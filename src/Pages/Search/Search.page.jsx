cecf56e961ccffaa50ccb72b068e62d749b37e41
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-12-07 00:32:42 +0530

    added pagination

--- src/Pages/Search/Search.page.jsx
+++ src/Pages/Search/Search.page.jsx
@@ -1,23 +1,42 @@
 import React , {useState}from 'react'
 import Card from '../../Components/Card/Card.component'
 import Fetcher from '../../Components/Fetcher'
-
+import ReactPaginate from 'react-paginate';
+import '../../Components/Pagination/Pagination.styles.css'
 function Search() {
     const [jobs,setJobs] = useState([])
+    const [pageNumber, setPageNumber] = useState(0)
+    const   itemsPerPage = 20;
+    const pagesVisited = pageNumber * itemsPerPage
+    // function  for pagination
+    const displayItems = jobs.slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map( jobDetail =><Card jobDetail={jobDetail} key={jobDetail.id}/>)  
+    var pageCount = Math.ceil(jobs.length / itemsPerPage)
+    const changePage = ({selected}) =>
+    {
+        setPageNumber(selected)
+    }
     return (
-        <div>
+        <div className='SearchPage'>
             This is the search page
             <Fetcher jobs ={jobs} setJobs = {setJobs}/>
+            <div>
             {
-                jobs[0] ?
-                jobs.map(jobDetail =>
-                    {
-                        console.log('here are the jobs',jobDetail);
-                    return(
-                        <Card jobDetail={jobDetail} key={jobDetail.id}/>
-                        )
-                }) : null
-            }
+                jobs ?
+                displayItems: null
+            } 
+            </div>
+            <ReactPaginate
+                previousLabel = {"previous"}
+                nextLabel = {"next"}
+                pageCount = {pageCount}
+                onPageChange = {changePage}
+                containerClassName = {"paginationBtns"}
+                previousLinkClassName = {"previousBtn"}
+                nextLinkClassName = {"nextBtn"}
+                disabledClassName = {"paginationDisabled"}
+                activeClassName  = {"paginationActive"}
+            />
         </div>
     )
 }
