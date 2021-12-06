2eb50e4e547761ef153e9186f01607178cb479e5
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-12-06 19:41:48 +0530

    added fetching mechanism, and local storage capability

--- src/Components/Fetcher.jsx
+++ src/Components/Fetcher.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Fetcher() {
+    const [jobs,setJobs] = useState([])
+    const fetchData = () =>{
+        // fetching from the the REST API
+
+        // axios.get('https://remotive.io/api/remote-jobs')
+        // .then(resp=> {
+        //     console.log(resp.data.jobs.splice(0,30))
+        //     localStorage.setItem("jobs",JSON.stringify(resp.data.jobs.splice(0,200)))
+        //     setJobs(resp.data.jobs)
+        // })
+        
+        // ---------------------------------------------------------------
+        //  to avoid overusing the API, the jobs have been stored to the localStorage of the browser
+        //  to enable smooth developing and testing 
+        var data = localStorage.getItem("jobs")
+        const jobsList = JSON.parse(data)
+        setJobs(jobsList)
+
+        // ------------------------------------------------------------------
+
+    }
     return (
         <div>
             This is the Fetcher Component
+            <button onClick={fetchData}> Fetch Data</button>
         </div>
     )
 }
