753b015c98d7c084c29a861b31ee33c994412ee1
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-12-09 15:55:04 +0530

    styled, added variables

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar/Navbar.component';
 import Home from './Pages/Home/Home.page';
 import Search from './Pages/Search/Search.page';
 import CardPage from './Pages/CardDisplay/Card.page';
+import About from './Pages/About/About.page';
+import Contact from './Pages/Contact/Contact.page';
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/search" element={<Search/>} />
           <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
           <Route path='/Job-:JobId' element={<CardPage/>} />
         </Routes>
       </Router>
